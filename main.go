50787e986a55152256328304c5954dfd63bfeb75
Dan Brown <dan@madebymany.co.uk>
2015-06-10 17:46:56 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	server := Server{
+		DbUrl: "",
+	}
+
+	strListenPort := os.Getenv("DYRWOOD_PORT")
+	if strListenPort == "" {
+		strListenPort = "51900"
+	}
+	listenPort64, err := strconv.ParseInt(strListenPort, 0, 64)
+	if err != nil {
+		log.Fatal("error getting port number: ", err)
+	}
+
+	server.Port = int(listenPort64)
+
+	err = server.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+type Server struct {
+	DbUrl string
+	Port  int
+
+	rpcServer  *rpc.Server
+	l          net.Listener
+	shouldStop bool
+	stopped    chan struct{}
+}
+
+func (self *Server) Serve() (err error) {
+	self.stopped = make(chan struct{}, 1)
+	defer func() {
+		self.stopped <- struct{}{}
+	}()
+
+	db, err := sql.Open("postgres", self.DbUrl)
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	self.rpcServer = rpc.NewServer()
+
+	rpcHandler := NewDyrwood(db)
+	self.rpcServer.Register(rpcHandler)
+
+	self.l, err = net.Listen("tcp", fmt.Sprintf(":%d", self.Port))
+	if err != nil {
+		return
+	}
+
+	log.Printf("dyrwood listening on %d...", self.Port)
+
+	var conn net.Conn
+	for {
+		conn, err = self.l.Accept()
+
+		if err != nil {
+			if self.shouldStop {
+				err = nil
+			}
+			return
+		}
+
+		go self.rpcServer.ServeCodec(jsonrpc.NewServerCodec(conn))
+	}
+}
+
+func (self *Server) Stop() (err error) {
+	self.shouldStop = true
+	err = self.l.Close()
+	if err != nil {
+		return err
+	}
+	_ = <-self.stopped
+	return nil
+}
---
+++ svc.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/lib/pq"
+	"log"
+	"time"
+)
+
+type Dyrwood struct {
+	db              *sql.DB
+	refreshViewChan chan string
+	refreshers      map[string]chan struct{}
+	refreshDelay    time.Duration
+}
+
+type RefreshMaterializedViewRequest struct {
+	ViewName string `json:"view_name"`
+}
+
+func NewDyrwood(db *sql.DB) (out *Dyrwood) {
+	out = &Dyrwood{
+		db:              db,
+		refreshViewChan: make(chan string),
+		refreshers:      make(map[string]chan struct{}),
+		refreshDelay:    10 * time.Second,
+	}
+
+	go out.refreshViewListener()
+
+	return
+}
+
+func (self *Dyrwood) RefreshMaterializedView(req *RefreshMaterializedViewRequest, res *bool) error {
+	self.refreshViewChan <- req.ViewName
+	*res = true
+	return nil
+}
+
+func (self *Dyrwood) refreshViewListener() {
+	var refresher chan struct{}
+	var ok bool
+
+	for viewName := range self.refreshViewChan {
+		log.Printf("refresh for '%s' requested", viewName)
+		refresher, ok = self.refreshers[viewName]
+		if !ok {
+			refresher = make(chan struct{}, 1)
+			go self.refresher(viewName, refresher)
+			self.refreshers[viewName] = refresher
+			log.Printf("started refresher for '%s'", viewName)
+		}
+
+		// non-blocking channel send
+		select {
+		case refresher <- struct{}{}:
+		default:
+		}
+	}
+}
+
+func (self *Dyrwood) refresher(viewName string, inChan chan struct{}) {
+	var lastRefreshed, nextRefresh, now time.Time
+	var err error
+
+	for _ = range inChan {
+		now = time.Now()
+		nextRefresh = lastRefreshed.Add(self.refreshDelay)
+		if now.Before(nextRefresh) {
+			log.Printf("sleeping for '%s'", viewName)
+			time.Sleep(nextRefresh.Sub(now))
+		}
+
+		_, err = self.db.Exec(fmt.Sprintf(
+			`REFRESH MATERIALIZED VIEW CONCURRENTLY %s`,
+			pq.QuoteIdentifier(viewName)))
+		if err == nil {
+			log.Printf("refreshed view '%s'", viewName)
+		} else {
+			log.Printf("error refreshing view '%s': '%s'", viewName, err)
+		}
+
+		lastRefreshed = time.Now()
+	}
+}
